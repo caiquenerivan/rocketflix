32b639ffb96eab80d585479049f20d7a5de71d43
Caique Nerivan <caiquenerivansantos@gmail.com>
2022-02-07 00:04:46 -0300

    primeiro commit

---
+++ api.js
@@ -0,0 +1,4 @@
+export const API_KEY = 'api_key=5b9fbb16a34aeb4eaccaa462fd30dd7a';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const language = 'language=pt-BR';
---
+++ main.js
@@ -0,0 +1,61 @@
+import {
+  API_KEY, BASE_URL,
+  IMG_URL,
+  language,
+} from './api.js'
+
+const ENCONTRAR_FILME = document.getElementById("encontrarFilme");
+const INSERT_IMG = document.getElementById("posterFilme");
+const INSERT_TITLE = document.getElementById("tituloFilme");
+const INSERT_DESC = document.getElementById("descFilme");
+
+var data;
+var image;
+var title;
+var desc;
+
+const basicFetch = async (endpoint) => {
+  const req = await fetch(`${BASE_URL}${endpoint}?&${language}&${API_KEY}`)
+  const json = await req.json();
+  data = JSON.stringify(json);
+
+  image = `${IMG_URL}${json.poster_path}`;
+  title = json.title;
+  desc = json.overview;
+
+  return [image, title, desc];
+}
+
+function findMovie(num){
+  basicFetch(num);
+
+  if(!data){
+
+    image = url('./Poster.png');
+    title = "Ops, hoje não é dia de assistir filme. Bora codar!";
+
+    return{image, title};
+
+  } else {
+
+    return {image, title, desc};
+
+  }
+  console.log(image);
+  
+}
+
+function appendMovie(){
+  const dados = findMovie(Math.floor(Math.random() * 900 + 1));
+
+  console.log(dados);
+}
+
+ENCONTRAR_FILME.addEventListener("click", function(){
+  appendMovie();
+});
+
+
+
+
+
